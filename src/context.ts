c45c0a680346e4f7813869de39231a9bb776fb85
Ross Bratton <bratton.ross@gmail.com>
2020-08-22 22:36:20 +0100

    feat: support kebab and capital cased components

--- src/context.ts
+++ src/context.ts
@@ -2,14 +2,22 @@ import { ComponentsInfo, ComponentsImportMap, Options } from './types'
 
 export class Context {
   importMap: ComponentsImportMap = {}
-  components: ComponentsInfo[] = []
   _searchingPromise?: Promise<any>
 
+  private _components: ComponentsInfo[] = []
   private importMapPromises: Record<string, [(null | Promise<string[]>), (null | ((result: string[]) => void))]> = {}
 
   constructor(
     public readonly options: Options,
   ) {}
+
+  get components() {
+    return this._components
+  }
+
+  set components(components: ComponentsInfo[]) {
+    this._components = components.map(([name, path]) => [capitalize(camelize(name)), path])
+  }
 
   async getImportMap(key: string) {
     if (this.importMap[key])
@@ -27,8 +35,17 @@ export class Context {
   }
 
   setImportMap(key: string, names: string[]) {
-    this.importMap[key] = names
+    const casedNames = names.map(name => capitalize(camelize(name)))
+    this.importMap[key] = casedNames
     if (this.importMapPromises[key])
-      this.importMapPromises[key][1]?.(names)
+      this.importMapPromises[key][1]?.(casedNames)
   }
+}
+
+function camelize(str: string) {
+  return str.replace(/-(\w)/g, (_, c) => (c ? c.toUpperCase() : ''))
+}
+
+function capitalize(str: string) {
+  return str.charAt(0).toUpperCase() + str.slice(1)
 }
