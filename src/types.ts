da1f5a404e8c1aeafbba4dd22e852c790f0386ae
Subwaytime <leon.l@nophase.de>
2020-10-28 16:08:08 +0100

    Update Types
    Change "dirs" to "include_dirs" (easier to understand)
    Add Namespaces as array filled with strings
    Add allowFolderNames as boolean

--- src/types.ts
+++ src/types.ts
@@ -6,22 +6,40 @@ export interface Options {
    * Relative paths to the directory to search for components.
    * @default 'src/components'
    */
-  dirs: string | string[]
+  include_dirs: string | string[]
+
+  /**
+   * Directory names that are ignored as Component names.
+   * Only works if Foldernames are allowed
+   * @default "['global', 'partials']"
+   */ 
+  namespaces: string[]
+
   /**
    * Valid file extensions for components.
    * @default ['vue']
    */
   extensions: string | string[]
+
   /**
    * Search for subdirectories
    * @default true
    */
   deep: boolean
+
+  /**
+   * Allow directories as Names for components
+   * @default false
+   */
+
+  allowFolderNames: boolean
+
   /**
    * Path alias, same as what you passed to vite root config
    * @default {}
    */
   alias: Record<string, string>
+  
   /**
    * Root path of Vite project.
    * @default 'process.cwd()'
