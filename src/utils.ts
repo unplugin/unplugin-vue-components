42f88a142a68eacb1411a4e4a04947b6dd8a2116
Subwaytime <leon.l@nophase.de>
2020-10-29 13:50:00 +0100

    Update Utils Functions
    Add Split, Join and isEmpty Helpers
    Update getNameFromFilePath to support new Features: Namespacing and allowFolderNames
    Fix Windows Issue with "index-named" components

--- src/utils.ts
+++ src/utils.ts
@@ -20,6 +20,22 @@ export function toArray<T>(arr: T | T[]): T[] {
   return [arr]
 }
 
+export function split(str: string, sep: string) {
+    return str == '' ? [] : str.split(sep);
+}
+
+export function join(arr: string[], sep: string) {
+	return arr == null ? '' : Array.prototype.join.call(arr, sep);
+}
+
+export function isEmpty(value: any) {
+    if (!value || value === null || value === undefined || (Array.isArray(value) && Object.keys(value).length <= 0)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
 export function matchGlobs(filepath: string, globs: string[]) {
   for (const glob of globs) {
     if (minimatch(filepath, glob))
@@ -28,16 +44,50 @@ export function matchGlobs(filepath: string, globs: string[]) {
   return false
 }
 
-export function getNameFromFilePath(filePath: string): string {
-  const parsedFilePath = path.parse(filePath)
-  if (parsedFilePath.name === 'index') {
-    const filePathSegments = filePath.split(path.sep)
-    const parentDirName = filePathSegments[filePathSegments.length - 2]
-    if (parentDirName)
-      return parentDirName
-  }
-  return parsedFilePath.name
+export function getNameFromFilePath(filePath: string, includedFrom: string | string[], allowFolderNames: boolean, namespaces: string[]): string {
+    const parsedFilePath = path.parse(filePath);
+
+    let strippedPath = '';
+
+    // remove include directories from filepath
+
+    if(Array.isArray(includedFrom)) {
+        includedFrom.forEach((inc: string) => {
+            if(parsedFilePath.dir.includes(inc)) {
+                strippedPath = parsedFilePath.dir.replace(inc, '');
+            }
+        });
+    } else {
+        strippedPath = parsedFilePath.dir.replace(includedFrom, '');  
+    }
+
+    let folders = split(strippedPath.slice(1), "/");
+    let filename = parsedFilePath.name;
+
+    // set parent directory as filename if it is index 
+    if(filename === 'index') {
+        filename = `${folders.slice(-1)[0]}`;
+        return filename;
+    }
+
+    if (allowFolderNames) {
+        // remove namesspaces from folder names
+        if (namespaces.some((name: string) => folders.includes(name))) {
+            folders = folders.filter((f) => !namespaces.includes(f));
+        }
+
+        if(!isEmpty(folders)) {
+            // add folders to filename
+            filename = `${join(folders, '')}${filename}`;
+        }
+        
+        return filename;
+    }
+
+    return filename
 }
+
+
 
 export function resolveAlias(filepath: string, alias: Record<string, string>) {
   let result = filepath
