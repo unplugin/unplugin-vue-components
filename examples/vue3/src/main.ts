1841ca3cf115cd489aaaf69204d33f0a1d58447c
shinji <me@shinji.me>
2021-04-17 04:19:14 +0800

    feat: modify vant resolver (#38)

--- examples/vue3/src/main.ts
+++ examples/vue3/src/main.ts
@@ -1,6 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css'
-import 'vant/lib/index.css'
 
 createApp(App).mount('#app')
--- src/resolvers/vant.ts
+++ src/resolvers/vant.ts
@@ -1,11 +1,28 @@
 import { ComponentResolver } from '../types'
+import { kebabCase } from '../utils'
+
+export interface VantResolverOptions {
+  /**
+   * import style along with components
+   *
+   * @default true
+   */
+  importStyle?: boolean
+}
 
 /**
  * Resolver for Vant
  *
  * @link https://github.com/youzan/vant
  */
-export const VantResolver = (): ComponentResolver => (name: string) => {
-  if (name.startsWith('Van'))
-    return { importName: name.slice(3), path: 'vant' }
+export const VantResolver = (options: VantResolverOptions = {}): ComponentResolver => (name: string) => {
+  const { importStyle = true } = options
+  if (name.startsWith('Van')) {
+    const partialName = name.slice(3)
+    return {
+      importName: partialName,
+      path: 'vant/es',
+      sideEffects: importStyle ? `vant/es/${kebabCase(partialName)}/style` : undefined,
+    }
+  }
 }
