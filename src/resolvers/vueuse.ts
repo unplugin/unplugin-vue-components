953c7ba0c9b3472ff095faef840d3864b246fd70
wheat <jacobrclevenger@gmail.com>
2021-05-27 23:19:18 -0400

    feat(resolver): adds component resolver for VueUse components (#47)

---
+++ src/resolvers/vueuse.ts
@@ -0,0 +1,27 @@
+import { ComponentResolver } from '../types'
+
+let components: string[] | undefined
+
+/**
+ * Resolver for VueUse
+ *
+ * @link https://github.com/vueuse/vueuse
+ */
+export const VueUseComponentsResolver = (): ComponentResolver => (name: string) => {
+  if (!components) {
+    try {
+      /* eslint-disable @typescript-eslint/no-var-requires */
+      const indexesJson = require('@vueuse/core/indexes.json')
+      components = indexesJson
+        .functions
+        .filter((i: any) => i.component && i.name)
+        .map(({ name }: any) => name[0].toUpperCase() + name.slice(1))
+    }
+    catch (error) {
+      components = []
+    }
+  }
+
+  if (components && components.includes(name))
+    return { importName: name, path: '@vueuse/components' }
+}
