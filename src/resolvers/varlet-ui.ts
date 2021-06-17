b038ba97ef30fce6675f6cc204e16163bb32fe69
haoziqaq <357229046@qq.com>
2021-06-18 03:13:49 +0800

    feat: add Varlet UI resolver (#63)

---
+++ src/resolvers/varlet-ui.ts
@@ -0,0 +1,41 @@
+import { ComponentResolver } from '../types'
+import { kebabCase } from '../utils'
+
+export interface VarletUIResolverOptions {
+    /**
+     * import css along with components
+     *
+     * @default true
+     */
+    importCss?: boolean
+    /**
+     * import less along with components
+     *
+     * @default false
+     */
+    importLess?: boolean
+}
+
+/**
+ * Resolver for VarletUI
+ *
+ * @link https://github.com/haoziqaq/varlet
+ */
+export const VarletUIResolver = (options: VarletUIResolverOptions = {}): ComponentResolver => (name: string) => {
+    const {
+        importCss = true,
+        importLess
+    } = options
+    if (name.startsWith('Var')) {
+        const partialName = name.slice(3)
+        return {
+            importName: partialName,
+            path: '@varlet/ui/es',
+            sideEffects: importLess
+                ? `@varlet/ui/es/${kebabCase(partialName)}/style/less.js`
+                : importCss
+                    ? `@varlet/ui/es/${kebabCase(partialName)}/style`
+                    : undefined
+        }
+    }
+}
