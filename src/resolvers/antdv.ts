b944f8c077cc65d7648a7b682901b93390998994
nabaonan <nbn1209@126.com>
2021-06-26 21:09:29 +0800

    fix: update antdv version, fix style lost

--- src/resolvers/antdv.ts
+++ src/resolvers/antdv.ts
@@ -7,8 +7,13 @@ import { ComponentResolver } from '../types'
  *
  * @author @yangss3
  * @link https://antdv.com/
+ * antdv-version 2.2.0-beta.6
  */
 export const AntDesignVueResolver = (): ComponentResolver => (name: string) => {
   if (name.match(/^A[A-Z]/))
-    return { importName: name.slice(1), path: 'ant-design-vue/es' }
+    return {
+      importName: name.slice(1),
+      path: 'ant-design-vue/es',
+      sideEffects: "ant-design-vue/es/style",
+    }
 }
