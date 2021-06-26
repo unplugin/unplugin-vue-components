bd28136ea0b1ad33b9983129b685093cab8c6425
Anthony Fu <anthonyfu117@hotmail.com>
2021-06-27 05:04:20 +0800

    Update antdv.ts

--- src/resolvers/antdv.ts
+++ src/resolvers/antdv.ts
@@ -3,11 +3,12 @@ import { ComponentResolver } from '../types'
 /**
  * Resolver for Ant Design Vue
  *
+ * Requires ant-design-vue@v2.2.0-beta.6 or later
+ *
  * See https://github.com/antfu/vite-plugin-components/issues/26#issuecomment-789767941 for more details
  *
  * @author @yangss3
  * @link https://antdv.com/
- * antdv-version 2.2.0-beta.6
  */
 export const AntDesignVueResolver = (): ComponentResolver => (name: string) => {
   if (name.match(/^A[A-Z]/))
