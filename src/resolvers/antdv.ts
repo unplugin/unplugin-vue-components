7f74c4cb0fd6184781615f7cb08c51474d47b278
内小子 <897250040@qq.com>
2021-06-27 05:04:51 +0800

    fix: update antdv version, fix style lost (#74)
    Co-authored-by: Anthony Fu <anthonyfu117@hotmail.com>

--- src/resolvers/antdv.ts
+++ src/resolvers/antdv.ts
@@ -3,6 +3,8 @@ import { ComponentResolver } from '../types'
 /**
  * Resolver for Ant Design Vue
  *
+ * Requires ant-design-vue@v2.2.0-beta.6 or later
+ *
  * See https://github.com/antfu/vite-plugin-components/issues/26#issuecomment-789767941 for more details
  *
  * @author @yangss3
@@ -10,5 +12,9 @@ import { ComponentResolver } from '../types'
  */
 export const AntDesignVueResolver = (): ComponentResolver => (name: string) => {
   if (name.match(/^A[A-Z]/))
-    return { importName: name.slice(1), path: 'ant-design-vue/es' }
+    return {
+      importName: name.slice(1),
+      path: 'ant-design-vue/es',
+      sideEffects: "ant-design-vue/es/style",
+    }
 }
