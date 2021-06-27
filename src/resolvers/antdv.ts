d2770e0e4fd5985cf86f07abe5d742797e2e1bb5
nabaonan <nbn1209@126.com>
2021-06-27 18:52:18 +0800

    refactor: move the matcher outside
    load style on demand

--- src/resolvers/antdv.ts
+++ src/resolvers/antdv.ts
@@ -17,99 +17,96 @@ interface IMatcher {
   styleDir: string;
 }
 
+const matchComponents: IMatcher[] = [
+  {
+    pattern: /^Menu/,
+    styleDir: 'menu',
+  },
+  {
+    pattern: /^Layout/,
+    styleDir: 'menu',
+  },
+  {
+    pattern: /^Form/,
+    styleDir: 'form',
+  },
+  {
+    pattern: /^Table/,
+    styleDir: 'table',
+  },
+  {
+    pattern: /^Radio/,
+    styleDir: 'radio',
+  },
+  {
+    pattern: /^Dropdown/,
+    styleDir: 'dropdown',
+  },
+  {
+    pattern: /^Button/,
+    styleDir: 'button',
+  },
+  {
+    pattern: /^Checkbox/,
+    styleDir: 'checkbox',
+  },
+  {
+    pattern: /^List/,
+    styleDir: 'list',
+  },
+  {
+    pattern: /^Collapse/,
+    styleDir: 'collapse',
+  },
+  {
+    pattern: /^Descriptions/,
+    styleDir: 'descriptions',
+  },
+  {
+    pattern: /^Tabs/,
+    styleDir: 'tabs',
+  },
+  {
+    pattern: /^Mentions/,
+    styleDir: 'mentions',
+  },
+  {
+    pattern: /^Select/,
+    styleDir: 'select',
+  },
+  {
+    pattern: /^Mentions/,
+    styleDir: 'mentions',
+  },
+  {
+    pattern: /^Anchor/,
+    styleDir: 'anchor',
+  },
+  {
+    pattern: /^Typography/,
+    styleDir: 'typography',
+  },
+  {
+    pattern: /^TreeSelect/,
+    styleDir: 'tree-select',
+  },
+  {
+    pattern: /^Tree/,
+    styleDir: 'tree',
+  },
+  {
+    pattern: /^Step/,
+    styleDir: 'steps',
+  },
+  {
+    pattern: /^RangePicker|^WeekPicker|^MonthPicker/,
+    styleDir: 'date-picker',
+  },
+];
+
 export const AntDesignVueResolver = (): ComponentResolver => (name: string) => {
   if (name.match(/^A[A-Z]/)) {
     //Ant-Design-Vue
-    interface IMatcher {
-      pattern: RegExp;
-      styleDir: string;
-    }
-    const matchComponents: IMatcher[] = [
-      {
-        pattern: /^Menu/,
-        styleDir: 'menu',
-      },
-      {
-        pattern: /^Layout/,
-        styleDir: 'menu',
-      },
-      {
-        pattern: /^Form/,
-        styleDir: 'form',
-      },
-      {
-        pattern: /^Table/,
-        styleDir: 'table',
-      },
-      {
-        pattern: /^Radio/,
-        styleDir: 'radio',
-      },
-      {
-        pattern: /^Dropdown/,
-        styleDir: 'dropdown',
-      },
-      {
-        pattern: /^Button/,
-        styleDir: 'button',
-      },
-      {
-        pattern: /^Checkbox/,
-        styleDir: 'checkbox',
-      },
-      {
-        pattern: /^List/,
-        styleDir: 'list',
-      },
-      {
-        pattern: /^Collapse/,
-        styleDir: 'collapse',
-      },
-      {
-        pattern: /^Descriptions/,
-        styleDir: 'descriptions',
-      },
-      {
-        pattern: /^Tabs/,
-        styleDir: 'tabs',
-      },
-      {
-        pattern: /^Mentions/,
-        styleDir: 'mentions',
-      },
-      {
-        pattern: /^Select/,
-        styleDir: 'select',
-      },
-      {
-        pattern: /^Mentions/,
-        styleDir: 'mentions',
-      },
-      {
-        pattern: /^Anchor/,
-        styleDir: 'anchor',
-      },
-      {
-        pattern: /^Typography/,
-        styleDir: 'typography',
-      },
-      {
-        pattern: /^TreeSelect/,
-        styleDir: 'tree-select',
-      },
-      {
-        pattern: /^Tree/,
-        styleDir: 'tree',
-      },
-      {
-        pattern: /^Step/,
-        styleDir: 'steps',
-      },
-      {
-        pattern: /^RangePicker|^WeekPicker|^MonthPicker/,
-        styleDir: 'date-picker',
-      },
-    ];
     const importName = name.slice(1);
     let styleDir;
     const total = matchComponents.length;
