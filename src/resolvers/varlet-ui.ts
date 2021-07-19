627e8af7602f079abc11a430d01fd2711e21035c
Anthony Fu <anthonyfu117@hotmail.com>
2021-07-19 22:52:48 +0800

    fix(varlet): import style options changes

--- src/resolvers/varlet-ui.ts
+++ src/resolvers/varlet-ui.ts
@@ -3,15 +3,18 @@ import { kebabCase } from '../utils'
 
 export interface VarletUIResolverOptions {
   /**
-   * import css along with components
+   * import style along with components
    *
-   * @default true
+   * @default 'css'
+   */
+  importStyle?: boolean | 'css' | 'less'
+
+  /**
+   * @deprecated use `importStyle: 'css'` instead
    */
   importCss?: boolean
   /**
-   * import less along with components
-   *
-   * @default false
+   * @deprecated use `importStyle: 'less'` instead
    */
   importLess?: boolean
 }
@@ -21,21 +24,30 @@ export interface VarletUIResolverOptions {
  *
  * @link https://github.com/haoziqaq/varlet
  */
-export const VarletUIResolver = (options: VarletUIResolverOptions = {}): ComponentResolver => (name: string) => {
-  const {
-    importCss = true,
-    importLess,
-  } = options
-  if (name.startsWith('Var')) {
-    const partialName = name.slice(3)
-    const sideEffects = []
-    importCss && sideEffects.push(`@varlet/ui/es/${kebabCase(partialName)}/style`)
-    importLess && sideEffects.push(`@varlet/ui/es/${kebabCase(partialName)}/style/less.js`)
+export function VarletUIResolver(options: VarletUIResolverOptions = {}): ComponentResolver {
+  return (name: string) => {
+    const {
+      importStyle = 'css',
+      importCss = true,
+      importLess,
+    } = options
 
-    return {
-      importName: `_${partialName}Component`,
-      path: '@varlet/ui',
-      sideEffects,
+    if (name.startsWith('Var')) {
+      const partialName = name.slice(3)
+      const sideEffects = []
+
+      if (importStyle || importCss) {
+        if (importStyle === 'less' || importLess)
+          sideEffects.push(`@varlet/ui/es/${kebabCase(partialName)}/style/less.js`)
+        else
+          sideEffects.push(`@varlet/ui/es/${kebabCase(partialName)}/style`)
+      }
+
+      return {
+        importName: `_${partialName}Component`,
+        path: '@varlet/ui',
+        sideEffects,
+      }
     }
   }
 }
