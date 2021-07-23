2f3821eb38a818a2d694e35221923a79757ceba1
nabaonan <nbn1209@126.com>
2021-07-23 20:03:45 +0800

    feat: merge elementui

--- src/resolvers/element-plus.ts
+++ src/resolvers/element-plus.ts
@@ -1,31 +1,61 @@
-import { ComponentResolver } from '../types'
+import { ComponentResolver, SideEffectsInfo } from '../types'
+import { kebabCase } from '../utils'
 
 export interface ElementPlusResolverOptions {
   /**
-   * import style along with components
+   * import style css or sass with components
    *
-   * @default true
+   * @default 'css'
    */
-  importStyle?: boolean
+  importStyle?: boolean | 'css' | 'sass'
+  /**
+   * use ElementUI in vue2
+   *
+   * @default false
+   */
+  useElementUI?: boolean
 }
 
+function getSideEffects(
+  packageName: string,
+  componentName: string,
+  options: ElementPlusResolverOptions,
+): SideEffectsInfo | undefined {
+  const { importStyle = 'css' } = options
+  if (!importStyle)
+    return
+
+  if (importStyle === 'sass') {
+    return [
+      `${packageName}/packages/theme-chalk/src/base.scss`,
+      `${packageName}/packages/theme-chalk/src/${componentName}.scss`,
+    ]
+  }
+  else {
+    return [
+      `${packageName}/lib/theme-chalk/base.css`,
+      `${packageName}/lib/theme-chalk/${componentName}.css`,
+    ]
+  }
+}
 /**
- * Resolver for Element Plus
+ * Resolver for Element Plus or Element UI
  *
  * See https://github.com/antfu/vite-plugin-components/pull/28 for more details
  *
- * @author @develar
- * @link https://element-plus.org/#/en-US
+ * @author @develar @nabaonan
+ * @link https://element-plus.org/#/en-US for element-plus
+ * @link https://element.eleme.cn/#/zh-CN for element-ui
  */
 export function ElementPlusResolver(options: ElementPlusResolverOptions = {}): ComponentResolver {
   return (name: string) => {
-    const { importStyle = true } = options
-
-    if (name.startsWith('El')) {
-      const partialName = name[2].toLowerCase() + name.substring(3).replace(/[A-Z]/g, l => `-${l.toLowerCase()}`)
+    if (name.match(/^El[A-Z]/)) {
+      const { useElementUI = false } = options
+      const packageName = useElementUI ? 'element-ui' : 'element-plus'
+      const componentName = useElementUI ? kebabCase(name.slice(2)) : kebabCase(name)
       return {
-        path: `element-plus/es/el-${partialName}`,
-        sideEffects: importStyle ? `element-plus/packages/theme-chalk/src/${partialName}.scss` : undefined,
+        path: `${packageName}/lib/${componentName}`,
+        sideEffects: getSideEffects(packageName, componentName, options),
       }
     }
   }
