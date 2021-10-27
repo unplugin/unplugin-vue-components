e0c9aaf0b026c5053d27d715f8deadd1b406046a
hanlee <me@hanlee.co>
2021-10-28 04:20:46 +0900

    fix: set directives default to false

--- src/types.ts
+++ src/types.ts
@@ -127,10 +127,9 @@ export interface Options {
   /**
    * auto import for directives.
    *
-   * default: `true` for Vue 3, `false` for Vue 2
-   *
    * Babel is needed to do the transformation for Vue 2, it's disabled by default for performance concerns.
-   * @default undefined
+   * To install Babel, run: `npm install -D @babel/parser @babel/traverse`
+   * @default false
    */
   directives?: boolean
 }
