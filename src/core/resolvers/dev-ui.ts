ffe10146f2e3964531a424dfe8811a13bd681b58
banlify <banlify@163.com>
2021-11-29 18:47:05 +0800

    refactor: splited components and directives function

--- src/core/resolvers/dev-ui.ts
+++ src/core/resolvers/dev-ui.ts
@@ -17,37 +17,57 @@ export interface DevResolverOptions {
   directives?: boolean
 }
 
-function getDefaultConf() {
+interface Directive {
+  importName: string
+  styleName: string
+}
+
+export type Directives = Record<string, Directive>
+
+const LIB_NAME = 'vue-devui'
+
+const findStyle = (name: string) => `${LIB_NAME}/${name}/style.css`
+
+const effectMaps: Record<string, string> = {
+  'row,col': findStyle('grid'),
+  'aside,content,footer,header,layout': findStyle('layout'),
+  'overlay,fixed-overlay,flexible-overlay': findStyle('overlay'),
+}
+
+const effectKeys = Object.keys(effectMaps)
+
+function getSideEffects(name: string): string | undefined {
+  const match = effectKeys.find((key: string) => key.includes(name))
+  return (match && effectMaps[match]) || findStyle(name)
+}
+
+function componentsResolver(name: string, config: DevResolverOptions) {
+  if (!name.match(/^D[A-Z]/)) return
+
+  // Alert => alert; DatePicker => date-picker
+  const resolveId = kebabCase(name = name.slice(1))
+
   return {
-    directive: true,
-    importStyle: true,
+    path: LIB_NAME,
+    importName: name,
+    sideEffects: getSideEffects(resolveId),
   }
 }
 
-function getSideEffects(importName: string): string | undefined {
-  return `vue-devui/${importName}/style.css`
+function directivesResolver(name: string, config: DevResolverOptions) {
+  if (!config.directives) return
+
+  return {
+    importName: name,
+    path: `${LIB_NAME}/${name}`,
+  }
 }
 
-function componentResolver(name: string, config: DevResolverOptions) { }
+export function DevUiResolver(options: DevResolverOptions = {}): ComponentResolver[] {
+  const config = { directive: true, importStyle: true, ...options }
 
-function directiveResolver(name: string, config: DevResolverOptions) { }
-
-export function DevUiResolver(options: DevResolverOptions = {}): ComponentResolver {
-  return {
-    type: 'component',
-    resolve: (name: string) => {
-      if (name.match(/^D[A-Z]/)) {
-        const config = { ...getDefaultConf(), ...options }
-
-        name = name.slice(1)
-        const _dir = kebabCase(name)
-
-        return {
-          importName: name,
-          path: `vue-devui/${_dir}`,
-          ...config.importStyle && { sideEffects: getSideEffects(_dir) },
-        }
-      }
-    },
-  }
+  return [
+    { type: 'component', resolve: (name: string) => componentsResolver(name, config) },
+    { type: 'directive', resolve: (name: string) => directivesResolver(name, config) },
+  ]
 }
