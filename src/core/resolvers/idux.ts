123edef2ec82c84e3d611da157fe15bdcd1bd9a1
danranvm <danranvm@gmail.com>
2021-12-13 19:39:41 +0800

    feat(resolvers): sync idux

--- src/core/resolvers/idux.ts
+++ src/core/resolvers/idux.ts
@@ -1,23 +1,26 @@
 import { ComponentResolver } from '../../types'
 import { kebabCase } from '../utils'
 
-const cdkNames = [
-  'portal',
-  'resizable',
-  'virtual-list',
-]
-
-const kebabCaseDirnames = [
-  'virtual-list',
-  'auto-complete',
-  'back-top',
-  'date-picker',
-  'input-number',
-  'time-picker',
-  'tree-select',
-]
+const specialComponents: Record<string, string> = {
+  CdkVirtualScroll: 'scroll',
+  IxAutoComplete: 'auto-complete',
+  IxBackTop: 'back-top',
+  IxDatePicker: 'date-picker',
+  IxCol: 'grid',
+  IxRow: 'grid',
+  IxInputNumber: 'input-number',
+  IxTab: 'tabs',
+  IxTreeSelect: 'tree-select',
+  IxTimePicker: 'time-picker',
+}
 
 export interface IduxResolverOptions {
+  /**
+   * exclude components that do not require automatic import
+   *
+   * @default []
+   */
+  exclude?: string[]
   /**
    * import style along with components
    */
@@ -25,7 +28,7 @@ export interface IduxResolverOptions {
 }
 
 /**
- * Resolver for `@idux/cdk` and `@idux/components`
+ * Resolver for `@idux/cdk`, `@idux/components` and ``@idux/pro``
  *
  * @link https://idux.site
  */
@@ -33,30 +36,44 @@ export function IduxResolver(options: IduxResolverOptions = {}): ComponentResolv
   return {
     type: 'component',
     resolve: (name: string) => {
-      if (name.match(/^Ix[A-Z]/)) {
-        const { importStyle } = options
-        const compName = name.slice(2)
-        const kebabCaseName = kebabCase(compName)
-        const isCdk = cdkNames.includes(kebabCaseName)
-        const packageName = isCdk ? 'cdk' : 'components'
-        const dirname = getDirname(kebabCaseName)
-        const path = `@idux/${packageName}/${dirname}`
-        const sideEffects = isCdk || !importStyle ? undefined : `${path}/style/${importStyle === 'css' ? 'css' : 'index'}`
-
-        return { importName: name, path, sideEffects }
+      const { importStyle, exclude = [] } = options
+      if (exclude.includes(name)) {
+        return
       }
+
+      const packageName = getPackageName(name)
+      if (!packageName) {
+        return
+      }
+
+      let dirname = specialComponents[name]
+      if (!dirname) {
+        const nameIndex = packageName === 'pro' ? 2 : 1
+        dirname = kebabCase(name).split('-')[nameIndex]
+      }
+
+      const path = `@idux/${packageName}/${dirname}`
+
+      let sideEffects: string | undefined
+      if (packageName !== 'cdk' && importStyle) {
+        sideEffects = `${path}/style/themes/${importStyle === 'css' ? 'default_css' : 'default'}`
+      }
+
+      return { importName: name, path, sideEffects }
     },
   }
 }
 
-function getDirname(compName: string): string {
-  const dirname = kebabCaseDirnames.find(name => compName.startsWith(name))
-  if (dirname)
-    return dirname
+function getPackageName(name: string) {
+  let packageName: 'cdk' | 'components' | 'pro' | undefined
 
-  const [first] = compName.split('-')
-  if (first === 'row' || first === 'col')
-    return 'grid'
+  if (name.match(/^Cdk[A-Z]/)) {
+    packageName = 'cdk'
+  } else if (name.match(/^IxPro[A-Z]/)) {
+    packageName = 'pro'
+  } else if (name.match(/^Ix[A-Z]/)) {
+    packageName = 'components'
+  }
 
-  return first
+  return packageName
 }
