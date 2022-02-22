318b5f1321b7240a3e22875923d4c37783b3dfba
yuyang <uyarnchen@gmail.com>
2022-02-22 20:13:27 +0800

    fix: tdesign resolver

--- src/core/resolvers/tdesign.ts
+++ src/core/resolvers/tdesign.ts
@@ -28,7 +28,7 @@ function getSideEffects(importName: string, options: TDesignResolverOptions): Si
   if (!importStyle)
     return
 
-  if (fileName === 'config-provider')
+  if (['config-provider','icon'].includes(fileName))
     return
 
   if (fileName.includes('-') && fileName !== 'input-number') {
@@ -48,6 +48,9 @@ function getSideEffects(importName: string, options: TDesignResolverOptions): Si
   if (['aside', 'layout', 'header', 'footer', 'content'].includes(fileName))
     fileName = 'layout'
 
+  if (fileName === 'head-menu')
+    fileName = 'menu'
+  
   if (['option', 'option-group'].includes(fileName))
     fileName = 'select'
 
