022c68c5f595026f406a6431a282128187cb242c
sight <1453017105@qq.com>
2022-11-14 22:46:50 +0800

    feat(tdesign):add exclude option

---
+++ test/resolvers/tdesign.test.ts
@@ -0,0 +1,22 @@
+import { describe, expect, it } from 'vitest'
+import { TDesignResolver } from '../../src/resolvers'
+
+import type { ComponentResolveResult, ComponentResolverObject } from '../../src'
+
+describe('TDesignResolver', () => {
+  it('name matching string rule should not be resolved', async () => {
+    const resolver = TDesignResolver({ exclude: 'TString' }) as ComponentResolverObject
+    expect(resolver.resolve('TString')).toBeFalsy()
+  })
+
+  it('name matching RegExp rule should not be resolved', async () => {
+    const resolver = TDesignResolver({ exclude: /^TDoc[A-Z]/ }) as ComponentResolverObject
+    expect(resolver.resolve('TDocRegExp')).toBeFalsy()
+  })
+
+  it('name matching Array<string | RegExp> rule should not be resolved', async () => {
+    const resolver = TDesignResolver({ exclude: ['TString', /^TDoc[A-Z]/] }) as ComponentResolverObject
+    expect(resolver.resolve('TString')).toBeFalsy()
+    expect(resolver.resolve('TDocRegExp')).toBeFalsy()
+  })
+})
