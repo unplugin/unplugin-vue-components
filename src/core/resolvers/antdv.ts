1038da927e8b33fa7149533edf928ded1a04d404
JohnsonHuang <huangqihao@xh-online.com>
2023-11-14 14:11:09 +0800

    style: insert missing space

--- src/core/resolvers/antdv.ts
+++ src/core/resolvers/antdv.ts
@@ -182,7 +182,7 @@ export interface AntDesignVueResolverOptions {
    *
    * @default 'css'
    */
-  importStyle?: boolean | 'css' | 'less'| 'CSSinJS'
+  importStyle?: boolean | 'css' | 'less' | 'CSSinJS'
   /**
    * resolve `ant-design-vue' icons
    *
