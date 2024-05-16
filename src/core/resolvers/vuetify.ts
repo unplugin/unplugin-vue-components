f8f263986f9633444eaa1ffdb03278f8826b999a
赵日天 <61126721+TrueNine@users.noreply.github.com>
2024-05-17 03:39:48 +0800

    feat: add vuetify3Resolver labs support

--- src/core/resolvers/vuetify.ts
+++ src/core/resolvers/vuetify.ts
@@ -15,17 +15,30 @@ export function VuetifyResolver(): ComponentResolver {
   }
 }
 
+const _vLabsComponentNames = [
+  'VCalendar',
+  'VNumberInput',
+  'VPicker',
+  'VDateInput',
+  'VPullToRefresh',
+  'VSnackbarQueue',
+  'VStepperVertical',
+  'VTimePicker',
+  'VTreeview'
+]
+
 /**
  * Resolver for Vuetify 3 Beta
  *
  * @link https://github.com/vuetifyjs/vuetify
+ * @link https://vuetifyjs.com/en/labs/introduction/
  */
-export function Vuetify3Resolver(): ComponentResolver {
+export function Vuetify3Resolver(useLabsComponent?: boolean = false): ComponentResolver {
   return {
     type: 'component',
     resolve: (name: string) => {
       if (name.match(/^V[A-Z]/))
-        return { name, from: 'vuetify/components' }
+        return { name, from: useLabsComponent && _vLabsComponentNames.includes(name) ? 'vuetify/labs/components' : 'vuetify/components' }
     },
   }
 }
