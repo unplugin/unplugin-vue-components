3e2c889b07fe9e35cac6b7403bbd9434af261a7c
Issayah <github.me.mzu5a@simplelogin.com>
2024-06-01 02:07:11 -0500

    feat: deprecate bvn resolver (#759)

--- src/core/resolvers/bootstrap-vue.ts
+++ src/core/resolvers/bootstrap-vue.ts
@@ -90,6 +90,8 @@ export function BootstrapVueResolver(_options: BootstrapVueResolverOptions = {})
  * Resolver for BootstrapVueNext
  *
  * @link https://github.com/bootstrap-vue/bootstrap-vue-next
+ *
+ * @deprecated use `import { BootstrapVueNextResolver } from 'bootstrap-vue-next'` instead
  */
 export function BootstrapVueNextResolver(_options: BootstrapVueResolverOptions = {}): Array<ComponentResolver> {
   const options = { directives: true, ..._options }
