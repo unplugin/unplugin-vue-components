384429f911c3bd2809abeafc159325897f9dffe9
IlyaL <ilyaliao324@gmail.com>
2025-07-13 23:32:09 +0900

    test

--- test/search.test.ts
+++ test/search.test.ts
@@ -18,7 +18,7 @@ describe('search', () => {
     ctx.setRoot(root)
     ctx.searchGlob()
 
-    expect(ctx.options.globs).toMatch('D:/a/unplugin-vue-components/unplugin-vue-components/examples/vite-vue3/src/components/**/*.vue')
+    expect(ctx.options.globs).toMatchSnapshot()
     expect(cleanup(ctx.componentNameMap)).toMatchSnapshot()
   })
 
