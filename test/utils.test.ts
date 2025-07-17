d824eb77e86c77be16d01908588037e3808c9ad9
IlyaL <ilyaliao324@gmail.com>
2025-07-17 08:35:25 +0800

    fix: escape special characters in glob patterns (#858)

--- test/utils.test.ts
+++ test/utils.test.ts
@@ -1,6 +1,6 @@
 import type { ResolvedOptions } from '../src'
 import { describe, expect, it } from 'vitest'
-import { getNameFromFilePath } from '../src/core/utils'
+import { escapeSpecialChars, getNameFromFilePath } from '../src/core/utils'
 
 describe('getNameFromFilePath', () => {
   const options: Partial<ResolvedOptions> = {
@@ -18,5 +18,11 @@ describe('getNameFromFilePath', () => {
   it('special char', () => {
     const inComponentFilePath = '/src/components/[a1]/b_2/c 3/d.4/[...ef]/ghi.vue'
     expect(getNameFromFilePath(inComponentFilePath, options as ResolvedOptions)).toBe('a1-b2-c3-d4-ef-ghi')
+  })
+})
+
+describe('escapeSpecialChars', () => {
+  it('should escape parentheses', () => {
+    expect(escapeSpecialChars('component()')).toBe('component\\(\\)')
   })
 })
