035a4d361b90c9c7706b7f937abb73b4ac9b4dd4
Kevin Deng <sxzz@sxzz.moe>
2026-01-15 06:30:43 +0800

    chore: upgrade tsdown

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -2,5 +2,22 @@ import { defineConfig } from 'tsdown'
 
 export default defineConfig({
   entry: ['src/*.ts'],
-  format: ['esm'],
+  exports: true,
+  inlineOnly: [
+    '@antfu/utils',
+    'compare-versions',
+    'estree-walker',
+    'minimatch',
+    '@isaacs/brace-expansion',
+    '@isaacs/balanced-match',
+  ],
+  external: [
+    // dts
+    'webpack',
+    'esbuild',
+    'rollup',
+    'rolldown',
+    'vite',
+    '@nuxt/schema',
+  ],
 })
