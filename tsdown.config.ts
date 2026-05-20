bbf51c36ee5b1b261f3f5f30a422b597c52a6dba
Anthony Fu <github@antfu.me>
2026-05-20 13:32:35 +0900

    chore: upgrade pnpm to v11, bump deps
    - Bump packageManager to pnpm@11.1.3 and regenerate lockfile
    - Migrate tsdown `external`/`inlineOnly` to `deps.neverBundle`/`onlyBundle`
    - pnpm v11 auto-added `minimumReleaseAgeExclude` entries to workspace yaml
    - Refresh dependency caret ranges to latest installed versions

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -4,14 +4,16 @@ export default defineConfig({
   entry: ['src/*.ts'],
   dts: { tsgo: true },
   exports: true,
-  inlineOnly: ['@antfu/utils', 'compare-versions'],
-  external: [
-    // dts
-    'webpack',
-    'esbuild',
-    'rollup',
-    'rolldown',
-    'vite',
-    '@nuxt/schema',
-  ],
+  deps: {
+    onlyBundle: ['@antfu/utils', 'compare-versions'],
+    neverBundle: [
+      // dts
+      'webpack',
+      'esbuild',
+      'rollup',
+      'rolldown',
+      'vite',
+      '@nuxt/schema',
+    ],
+  },
 })
